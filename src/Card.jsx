becba602381c5446584cc8f6298d3205baf7037a
Wessley Silva <wessley.silva@outlook.com.br>
2024-04-22 20:08:21 -0300

    adding img, create card component, removing unused files

---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card() {
+    return (<>
+        <img src="/the-office.jpeg" alt=""/>
+        <h1>Pôster: The Office(2005)</h1>
+        <p>Um poster decorativo épico da série The Office, em tamanho A3. Uma ótima recordação de um dos mais icônicos seriados de humor de todos os tempos. Com suas muitas temporadas, divertiu, e diverte, muitos públicos até o dia de hoje. Não perca a chance de imortalizar a sua série favorita.</p>
+        <button>Comprar Agora</button>
+        </>
+        )
+    }
