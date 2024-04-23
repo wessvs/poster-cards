3effa57c43382a8014f4630ec5154b6ab85e43d9
Wessley Silva <wessley.silva@outlook.com.br>
2024-04-23 18:25:21 -0300

    create a button component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import posterImg from "../../assets/the-office.jpg" 
+import Button from "../Button"
 import styles from './styles.module.css'
 
 
@@ -11,7 +12,7 @@ export default function Card() {
             <div>
                 <h2>Pôster: The Office(2005)</h2>
                 <p>Um poster decorativo épico da série The Office, em tamanho A3. Uma ótima recordação de um dos mais icônicos seriados de humor de todos os tempos. Com suas muitas temporadas, divertiu, e diverte, muitos públicos até o dia de hoje. Não perca a chance de imortalizar a sua série favorita.</p>
-                <button>Comprar Agora</button>
+                <Button />
             </div>
         </div>
         </>
